804c262ad6fdb126d64841588aa6e3724e5341e7
kfedus6 <feduskolya@gmail.com>
2021-12-02 19:02:59 +0200

    Sample code

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+//window.alert("test")
+//window.document.write("test")
+/*
+function display() {
+   console.log("hello")
+}
+
+window.display()
+*/
+
+//console.log(document.body.style.background = "red")
+
+//console.log(location.href)
+//location.href = "https://app.diagrams.net/";
+
+/*
+document.title = "Lesson";
+console.log(document.body.childNodes)
+
+for (let i = 0; i < document.body.childNodes.length; i++) {
+   console.log(document.body.childNodes[i])
+}
+
+//firstChild lastChild
+
+console.log(document.body.firstChild, document.body.lastChild)
+
+
+for (item of document.body.childNodes) {
+   console.log(item)
+}
+*/
+
+//console.log(document.body.parentNode === document.documentElement)
+
+//console.log(document.head.nextSibling)
+
+
+// for (let elem of document.body.children) {
+//    for (let l of elem.children) {
+//       l.style.background = 'red';
+//    }
+// }
