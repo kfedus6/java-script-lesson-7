c8c3a3929e107d8bcf447b6d1d8c4b0668fae409
kfedus6 <feduskolya@gmail.com>
2021-12-04 19:11:30 +0200

    Sample code

--- script.js
+++ script.js
@@ -44,10 +44,80 @@ for (item of document.body.childNodes) {
 
 //DZ\\
 
-for (elem of document.body.children) {
-   console.log(elem)
-   for (l of elem.children) {
-      console.log(l)
+// for (elem of document.body.children) {
+//    console.log(elem)
+//    for (l of elem.children) {
+//       console.log(l)
+//    }
+// }
+
+
+//console.log(document.body.firstElementChild);
+//console.log(document.body.children[1].lastElementChild)
+
+
+//getElement, querySelector
+
+
+/*
+let elem = document.body.firstElementChild
+elem.style.color = "red";
+console.log(elem);
+*/
+/*
+unickBlock = document.getElementById('unik_block');
+unickBlock.style.fontSize = "25px";
+//let cla = prompt();
+//let sty = prompt();
+
+
+
+let setText = (a, b) => {
+   const elem = document.getElementsByClassName(a);
+   for (item of elem) {
+      item.style.color = b;
    }
 }
+setText(cla, sty)
 
+*/
+
+//document.querySelector('.container').style.color = 'red';
+//document.querySelector('.container ul').style.color = 'green';
+
+/*
+let container = document.getElementsByClassName('container')[0];
+elem = container.children;
+for (item of elem) {
+   console.log(item.textContent)
+}
+*/
+
+/*
+let elem = document.querySelectorAll('.container ul li');
+for (item of elem) {
+   console.log(item.innerHTML)
+}
+*/
+
+// let elem = document.getElementsByName('search');
+// elem[0].style.fontSize = "30px";
+// elem[0].style.background = "red";
+
+// for (item of elem) {
+//    console.log(item)
+//    item[0].style.background = "yellow";
+//    item[1].style.background = "green";
+// }
+// console.log(elem)
+
+
+// let elem = document.getElementById('age-table');
+// elem.style.background = 'red';
+// console.log(elem)
+
+// let elem = document.querySelectorAll('#age-list label');
+// for (item of elem) {
+//    item.style.color = 'red';
+// }
+// console.log(elem);
